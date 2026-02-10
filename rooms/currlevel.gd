20eda3f4b11e78e57d07bdac678ac589ff7443cd
ConnerGordon <2260684@flourbluffschools.org>
2026-02-10 12:42:50 -0600

    full mapping
    created the mapping in full, no invisible walls. random selection implemented into manager

--- rooms/currlevel.gd
+++ rooms/currlevel.gd
@@ -7,8 +7,12 @@ var next
 func _ready() -> void:
 	var choose = randf_range(0,3)
 	
-	if (int)choose == 0:
-		next = preload()
+	if int(choose) == 0:
+		next = preload("res://rooms/nextworldleft.tscn")
+	if int(choose) == 1:
+		next = preload("res://rooms/nextworldstraight.tscn")
+	if int(choose) == 2:
+		next = preload("res://rooms/nextworldright.tscn")
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
