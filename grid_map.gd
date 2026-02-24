cbdb0945612c736bd0ac0d1788aeff1c3a44a387
ConnerGordon <2260684@flourbluffschools.org>
2026-02-24 13:19:55 -0600

    movement smoothing and rotation
    smoothed out movement and added player rotation.

--- grid_map.gd
+++ grid_map.gd
@@ -15,13 +15,9 @@ func callspawn() -> void:
 			add_child(en)
 			while inv == false:
 				var mappout = local_to_map(Vector3(randf_range(-80,80),0,randf_range(-75,75)))
-				print(get_cell_item(mappout))
 				if get_cell_item(mappout) == 0:
 					en.position =  map_to_local(mappout)
-					print(en.position)
-					set_cell_item(mappout,-1)
+					set_cell_item(mappout,1)
 					Encount -= 1
 					inv = true
-					
-					print("woh")
 					
--- player/player.gd
+++ player/player.gd
@@ -4,16 +4,23 @@ class_name Player
 @onready var ui: CanvasLayer = $UI
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var label: Label = $UI/VBoxContainer/Label
+@onready var rotpiv: Node3D = $rotpiv
+@onready var rotspd = 15
 
 @onready var camera_3d: Camera3D = $Camera3D
 @onready var timer_1: Timer = $Timer1
 @onready var timer_2: Timer = $Timer2
 @onready var swingabl: Timer = $swingabl
+@onready var dashing: Timer = $dashing
 
 var camerabase
+var last := Vector3.BACK
 
-const SPEED = 20.0
+
+const SPEED = 15.0
+const accel = SPEED * 20.0/8.0
 const JUMP_VELOCITY = 4.5
+
 
 var sword = true
 var gun = false
@@ -36,9 +43,9 @@ func _physics_process(delta: float) -> void:
 	
 	
 	
-	#if camerabase != null:
-		#camera_3d.global_position = camerabase
-	#camera_3d.look_at(global_position)
+	if camerabase != null:
+		camera_3d.global_position = camerabase
+	camera_3d.look_at(global_position)
 	
 	
 	
@@ -52,17 +59,38 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	if Input.is_anything_pressed() == false:
+		velocity= velocity.clampf(-7.5,7.5)
 	
-		
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (camera_3d.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	var forward := camera_3d.global_basis.z
+	var right := camera_3d.global_basis.x
+	
+	var movedir := forward * input_dir.y + right * input_dir.x
+	movedir.y = 0.0
+	movedir = movedir.normalized()
+	
+	velocity = velocity.move_toward(movedir * SPEED, accel*delta )
+	if Input.is_action_just_pressed("dash") && dashing.is_stopped():
 		
+		velocity = velocity.move_toward(last * SPEED * 4, accel)
+		dashing.start()
+	
+	
+	
+	if movedir.length() > 0.2:
+		last = movedir
+	var target = Vector3.BACK.signed_angle_to(last,Vector3.UP)
+	rotpiv.global_rotation.y = lerp_angle(rotpiv.rotation.y, target, rotspd * delta)
+	
+	#if direction:
+		#velocity.x = direction.x * SPEED
+		#velocity.z = direction.z * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.z = move_toward(velocity.z, 0, SPEED)
+	
 	if sword:
 		if Input.is_action_just_pressed("attack") && swingabl.is_stopped():
 			if timer_2.is_stopped() == false:
@@ -94,7 +122,8 @@ func _physics_process(delta: float) -> void:
 	
 	
 	
-
+func _on_dashing_timeout() -> void:
+	velocity.clampf(-5,5)
 
 
 
